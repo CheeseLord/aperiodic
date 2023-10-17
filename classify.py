c9a6fd6a9f82819cf988fc5448fd74d797869106
Kyle Fox <fox_kyle@yahoo.com>
2023-10-17 16:22:30 -0400

    Classify again without bug.

--- classify.py
+++ classify.py
@@ -96,19 +96,13 @@ def isRepeating(shapes):
 
 
 if __name__ == '__main__':
-    """
     with open('shapes/allShapes.txt') as f:
         lines = f.readlines()
     shapes = [eval(l) for l in lines]
-    for shape in shapes:
+    for i, shape in enumerate(shapes):
         class_, size = classify(shape)
         className = str(class_).lower().split('.')[1]
         with open(f'shapes/{className}-{size}.txt', 'a') as f:
             f.write(f'{shape}\n')
-        print(className, size)
-    """
-
-    with open('shapes/invalid-2.txt') as f:
-        shapes = [eval(l) for l in f.readlines()]
-    print(classify(shapes[9]))
+        print(f'{i: 5d}', className, size)
 
