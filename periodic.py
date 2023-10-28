169220ed3e66d5dac07d7087d21890b40728a306
Kyle Fox <fox_kyle@yahoo.com>
2023-10-28 15:25:05 -0400

    Test periodicity wtih random sampling.

--- periodic.py
+++ periodic.py
@@ -20,12 +20,39 @@ def periodic2(shape):
     return False
 
 
+def periodicSampling(shape, period):
+    shape = np.array(shape)
+
+    variants = []
+    for rot in range(3):
+        for axes in itertools.product([-1, 1], repeat=3):
+            if np.prod(axes) == 1:
+                variants.append(np.roll(shape, rot, axis=2) * axes)
+            else:
+                variants.append(np.roll(shape[:, :, ::-1], rot, axis=2) * axes)
+
+    offsets = list(itertools.product(range(period), repeat=3))
+
+    for _ in range(10000):
+        shapes = []
+        for _ in range(period):
+            v = random.choice(variants).copy()
+            a, b, c = random.choice(offsets)
+            v += ((b + c, c + a, a + b), (0, 0, 0))
+            shapes.append([(tuple(x[0]), tuple(x[1])) for x in v])
+        if isRepeating(shapes):
+            return True
+    return False
+
+
 if __name__ == '__main__':
     with open('shapes/unknown.txt') as f:
         shapes = [eval(l) for l in f.readlines()]
     for i, shape in enumerate(shapes):
-        if periodic2(shape):
-            with open(f'shapes/working/periodic-2.txt', 'a') as f:
+        periodic = periodicSampling(shape, 4)
+        print(i, periodic)
+        if periodic:
+            with open(f'shapes/working/periodic-4.txt', 'a') as f:
                 f.write(f'{shape}\n')
         else:
             with open(f'shapes/working/unknown.txt', 'a') as f:
