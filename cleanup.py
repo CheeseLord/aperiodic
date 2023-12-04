aba2f8a297e3395c16d2ed1852e9a3f9872333d9
Kyle Fox <fox_kyle@yahoo.com>
2023-12-04 15:54:36 -0500

    Remove duplicates.

--- cleanup.py
+++ cleanup.py
@@ -1,7 +1,8 @@
 import os
+import re
 
 
-if __name__ == '__main__':
+def cleanup():
     with open('shapes/unknown.txt') as f:
         unknown = [eval(l) for l in f.readlines()]
     try:
@@ -22,4 +23,37 @@ if __name__ == '__main__':
         for shape in unknown:
             f.write(f'{shape}\n')
     os.replace(f'shapes/working/unknown.txt', f'shapes/unknown.txt')
+
+
+def correct():
+    seen = set()
+
+    for base in ['invalid-dfs', 'invalid-cover', 'periodic']:
+        names = [x for x in os.listdir('shapes') if x.startswith(base)]
+        names = sorted(names,
+            key=lambda x: int(re.match('[^\d]*(\d+)[^\d]*', x).groups()[0])
+        )
+        for name in names:
+            with open(f'shapes/{name}') as f:
+                shapes = [eval(l) for l in f.readlines()]
+            for shape in shapes:
+                t = tuple(shape)
+                if t in seen:
+                    continue
+                with open(f'shapes/working/{name}', 'a+') as f:
+                    f.write(f'{shape}\n')
+                seen.add(t)
+    with open(f'shapes/allShapes.txt') as f:
+        shapes = [eval(l) for l in f.readlines()]
+    for shape in shapes:
+        t = tuple(shape)
+        if t in seen:
+            continue
+        with open(f'shapes/working/unknown.txt', 'a+') as f:
+            f.write(f'{shape}\n')
+        seen.add(t)
+
+
+if __name__ == '__main__':
+    cleanup()
 
