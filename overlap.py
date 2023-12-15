64a314c1c77d2515f4cd0249b8d3a4d5a30bd347
Kyle Fox <fox_kyle@yahoo.com>
2023-12-15 12:10:08 -0500

    Find periodic blocks in large tilings.

--- overlap.py
+++ overlap.py
@@ -1,18 +1,9 @@
 import itertools
+import more_itertools
 import numpy as np
+import sympy
 
-
-def translate(shapes, offset):
-    other = np.array(shapes)
-    other[:, :, 0] -= offset
-    other = [
-        tuple(
-            tuple(
-                tuple(x) for x in widget
-            ) for widget in shape
-        ) for shape in other
-    ]
-    return other
+import periodic
 
 
 def randomTransformation(shapes):
@@ -28,50 +19,106 @@ def randomTransformation(shapes):
     return other
 
 
+def translate(shapes, offset):
+    other = np.array(shapes)
+    other[:, :, 0] += offset
+    other = [
+        [
+            tuple(
+                tuple(x) for x in widget
+            ) for widget in shape
+        ] for shape in other
+    ]
+    return other
+
+
 def overlap(shapes, other):
     shapes = {tuple(shape) for shape in shapes}
     other = {tuple(shape) for shape in other}
-    return list(shapes & other)
+    return [list(x) for x in shapes & other]
+
+
+def isRepeating(shapes):
+    # Find a basis for large repeats.
+    possible = [
+        offset for offset in itertools.product(range(-10, 11), repeat=3)
+        if sum(offset) % 2 == 0 and offset > (0, 0, 0)
+    ]
+    possible = sorted(possible, key=np.linalg.norm)
+    repeats = []
+    for offset in possible:
+        if len(overlap(shapes, translate(shapes, offset))) < len(shapes) // 3:
+            continue
+        mat = np.array(repeats + [offset])
+        if len(sympy.Matrix(mat).T.rref()[1]) > len(repeats):
+            repeats.append(offset)
+            if len(repeats) == 3:
+                break
+    if len(repeats) < 3:
+        return False, 0
+
+    # Get the fundamental region.
+    low = np.zeros(3, dtype=int)
+    high = np.zeros(3, dtype=int)
+    for subset in more_itertools.powerset(repeats):
+        corner = np.sum(subset, axis=0)
+        low = np.minimum(low, corner)
+        high = np.maximum(high, corner)
+    mat = np.array(repeats).T
+    possible = [
+        offset for offset in itertools.product(range(-10, 11), repeat=3)
+        if sum(offset) % 2 == 0
+    ]
+    centers = []
+    for center in possible:
+        coeffs = np.linalg.solve(mat, center)
+        if np.all((0 <= coeffs) & (coeffs < 1)):
+            centers.append(center)
+
+    # Find the tiles in the fundamental region
+    fundamental = []
+    for shape in shapes:
+        for center, _direction in shape:
+            if center in centers:
+                fundamental.append(shape)
+                break
+    for offsets in more_itertools.powerset(repeats):
+        if not offsets:
+            continue
+        result = fundamental
+        for offset in offsets:
+            result = translate(result, offset)
+        fundamental = [
+            shape for shape in fundamental
+            if shape not in overlap(fundamental, result)
+        ]
+
+    return periodic.isRepeating(fundamental), len(fundamental)
 
 
 if __name__ == '__main__':
-    import argparse
-    import matplotlib.pyplot as plt
+    import os
 
-    from display import drawShapes, drawHull
+    from geometry import orient
 
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('shapeFile', nargs='?')
-    args = parser.parse_args()
+    PATH = 'gallery/20k'
 
-    with open(args.shapeFile) as f:
-        shapes = [eval(l) for l in f.readlines()]
+    with open(f'{PATH}/unknown.txt') as f:
+        unknown = [eval(l) for l in f.readlines()]
 
-    while True:
-        other = randomTransformation(shapes)
-        thing = overlap(shapes, other)
-        if len(thing) >= 10:
-            break
-    print(len(shapes), len(thing))
-    print((np.array(other) - shapes)[0, 0, 0])
+    for name in sorted(os.listdir(PATH)):
+        with open(f'{PATH}/{name}') as f:
+            shapes = [eval(l) for l in f.readlines()]
 
-    """
-    other = translate(shapes, [0, 1, 1])
-    other = translate(shapes, [4, 0, 0])
-    other = translate(shapes, [0, 0, 2])
-    thing = overlap(shapes, other)
-    print(len(shapes), len(thing))
-    """
+        repeating, period = isRepeating(shapes)
+        if not repeating:
+            continue
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+        print(name, period)
+        index = int(name.split('.')[0][-3:])
+        s = unknown[index - 1]
 
-    # drawShapes(ax, thing)
-
-    drawHull(ax, shapes, 'r')
-    drawHull(ax, other, 'g')
-    drawHull(ax, thing, 'b')
-
-    plt.show()
+        with open(f'shapes/working/periodic-{period}', 'a') as f:
+            f.write(f'{s}\n')
 
