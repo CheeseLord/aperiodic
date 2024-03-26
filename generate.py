3bfce00eddfec9a0a7fe188c5e745fc83bd8a55a
Kyle Fox <fox_kyle@yahoo.com>
2024-03-25 23:28:52 -0400

    Regenerate list of shapes.

--- generate.py
+++ generate.py
@@ -4,46 +4,12 @@ import numpy as np
 from geometry import getNeighbors
 
 
-def generateShape():
+def generateAllShapes(n):
     shape = [
         ((0, 0, 0), (1, 0, 0)),
         ((0, 0, 0), (1, 1, 1)),
     ]
-    neighbors = [getNeighbors(x) for x in shape]
-    neighbors = neighbors[0] + neighbors[1]
-
-    o = 2
-    t = 3
-    while o or t:
-        i = np.random.randint(len(neighbors))
-        n = neighbors[i]
-        del neighbors[i]
-
-        inOct = 0 in n[1]
-
-        if n in shape:
-            continue
-        if inOct and o == 0:
-            continue
-        if not inOct and t == 0:
-            continue
-
-        if inOct:
-            o -= 1
-        else:
-            t -= 1
-        shape.append(n)
-        neighbors.extend(getNeighbors(n))
-
-    return shape
-
-
-def generateAllShapes():
-    shape = [
-        ((0, 0, 0), (1, 0, 0)),
-        ((0, 0, 0), (1, 1, 1)),
-    ]
-    shapes = _generateHelper(shape, 2, 3)
+    shapes = _generateHelper(shape, 3 * n - 1, 4 * n - 1)
     shapes = [list(y) for y in {tuple(sorted(x)) for x in shapes}]
     shapes = [list(y) for y in {tuple(makeCanonical(x)) for x in shapes}]
     return shapes
@@ -70,22 +36,24 @@ def _generateHelper(shape, o, t):
 
 
 def makeCanonical(shape):
-    best = sorted(shape)
+    best = None
     arr = np.array(shape)
 
     for sign in itertools.product([1, -1], repeat=3):
         for perm in itertools.permutations(range(3)):
             modified = arr[:, :, perm] * sign
+            modified[:, 0] -= np.min(modified[:, 0], axis=0)
             newShape = sorted([(tuple(x[0]), tuple(x[1])) for x in modified])
-            best = min(best, newShape)
-
-    # TODO: Translate each widget to (0, 0, 0).
+            if best is None:
+                best = newShape
+            else:
+                best = min(best, newShape)
 
     return best
 
 
 if __name__ == '__main__':
-    shapes = generateAllShapes()
+    shapes = generateAllShapes(1)
     with open('shapes/allShapes.txt', 'w') as f:
         f.writelines([f'{shape}\n' for shape in shapes])
     print(len(shapes))
