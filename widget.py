0a39e903e9902f3d94585e9e2f493e753be0dd39
Kyle Fox <fox_kyle@yahoo.com>
2024-06-03 15:52:36 -0400

    Create more general widget class.

---
+++ widget.py
@@ -0,0 +1,100 @@
+import itertools
+import numpy as np
+
+
+# Directions around a vertex.
+DIRECTIONS = [
+    (x, y, z) for x, y, z in itertools.product([1, 0, -1], repeat=3)
+    if (x + y + z) % 2 == 1
+]
+
+# Adjacent directions at the same vertex.
+_adjacentSameCenter = {
+    direction: [] for direction in DIRECTIONS
+}
+for direction in DIRECTIONS:
+    if 0 in direction:
+        continue
+    for otherDir in DIRECTIONS:
+        if sorted(np.abs(np.array(direction) - otherDir)) == [0, 1, 1]:
+            _adjacentSameCenter[direction].append(otherDir)
+            _adjacentSameCenter[otherDir].append(direction)
+
+# Adjacent directions around neighboring centers.
+_adjacentOtherCenter = {
+    direction: [] for direction in DIRECTIONS
+}
+for direction in DIRECTIONS:
+    if 0 in direction:
+        for i in range(3):
+            if direction[i] != 0:
+                continue
+            for j in [-1, 1]:
+                v = np.zeros(3, dtype=int)
+                v[i] = j
+                _adjacentOtherCenter[direction].append(
+                    (tuple(direction - v), tuple(v))
+                )
+    else:
+        for i in range(3):
+            v = np.ones(3, dtype=int) * -1
+            v[i] = 1
+            v *= direction
+            _adjacentOtherCenter[direction].append(
+                (tuple((direction - v) // 2), tuple(v))
+            )
+
+
+class Widget:
+    def __init__(self, center, direction):
+        self.center = center
+        self.direction = direction
+
+    def __eq__(self, other):
+        return self.center == other.center and self.direction == other.direction
+
+    def __hash__(self):
+        return hash((self.center, self.direction))
+
+    def __repr__(self):
+        return str((self.center, self.direction))
+
+    @property
+    def isOct(self):
+        return 0 in self.direction
+
+    @property
+    def isTet(self):
+        return 0 not in self.direction
+
+    @property
+    def neighbors(self):
+        neighbors = [
+            Widget(self.center, otherDir)
+            for otherDir in _adjacentSameCenter[self.direction]
+        ]
+        neighbors += [
+            Widget(tuple(np.array(self.center) + otherCenter), otherDir)
+            for otherCenter, otherDir in _adjacentOtherCenter[self.direction]
+        ]
+        return neighbors
+
+    def translate(self, offset):
+        return Widget(tuple(np.array(self.center) + offset), self.direction)
+
+
+def getWidgets(n):
+    widgets = []
+    used = set()
+    toExpand = [Widget((0, 0, 0), (1, 0, 0))]
+    while len(widgets) < n:
+        w = toExpand[0]
+        toExpand = toExpand[1:]
+        if w in used:
+            continue
+        widgets.append(w)
+        used.add(w)
+        toExpand.extend([x for x in w.neighbors if x not in used])
+
+    return widgets
+
