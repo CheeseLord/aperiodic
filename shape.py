1fca64607a4109bf004fdd82a31b560bc953e88c
Kyle Fox <fox_kyle@yahoo.com>
2024-06-18 14:49:30 -0400

    First pass at orienting faces.

--- shape.py
+++ shape.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 
 from widget import Widget
@@ -44,11 +45,24 @@ class Shape:
         return np.array([tuple(w) for w in self.widgets])
 
     def translate(self, offset):
-        return Shape([w.translate(offset) for w in self])
+        if self.faces is None:
+            newFaces = None
+        else:
+            newFaces = {
+                (w.translate(offset), x.translate(offset)): label
+                for (w, x), label in self.faces.items()
+            }
+
+        return Shape([w.translate(offset) for w in self], newFaces)
 
     def orient(self, target, orientation):
         arr = np.array(self)
+        faceArr = []
+        for w, x in self.faces:
+            faceArr += [tuple(w), tuple(x)]
+        faceArr = np.array(faceArr)
 
+        # TODO: Unify this.
         if target.isOct:
             index, rotation = divmod(orientation, 4)
             index += 1
@@ -59,28 +73,27 @@ class Shape:
                     break
 
             arr[:, 0] -= widget.center
+            faceArr[:, 0] -= widget.center
 
+            # TODO: Make this a matrix.
             # Rotate the direction onto the target.
             axis = np.argmax(np.abs(widget.direction))
             targetAxis = np.argmax(np.abs(target.direction))
             arr = np.roll(arr, targetAxis - axis, axis=2)
+            faceArr = np.roll(faceArr, targetAxis - axis, axis=2)
             if widget.direction[axis] != target.direction[targetAxis]:
                 arr[:, :, targetAxis] *= -1
                 arr[:, :, (targetAxis + 1) % 3] *= -1
+                faceArr[:, :, targetAxis] *= -1
+                faceArr[:, :, (targetAxis + 1) % 3] *= -1
 
             # Use the Rodrigues formula to rotate axes.
             x, y, z = target.direction
             mat = np.array([
-                [1 - y * y - z * z, x * y - z, x * z + y],
-                [x * y + z, 1 - x * x - z * z, y * z - x],
-                [x * z - y, y * z + x, 1 - x * x - y * y],
-            ])
-            rot = np.linalg.matrix_power(mat, rotation)
-            arr = np.matmul(arr, rot)
-
-            arr[:, 0] += target.center
-
-            return Shape(arr)
+                [x * x, -z, y],
+                [z, y * y, -x],
+                [-y, x, z * z],
+            ], dtype=int)
 
         else:
             index, rotation = divmod(orientation, 3)
@@ -92,12 +105,17 @@ class Shape:
                     break
 
             arr[:, 0] -= widget.center
+            faceArr[:, 0] -= widget.center
 
+            # TODO: Make this a matrix.
             # Rotate the direction onto the target.
             arr *= widget.direction
+            faceArr *= widget.direction
             if np.prod(widget.direction) * np.prod(target.direction) == -1:
                 arr = arr[:, :, [1, 0, 2]]
+                faceArr = faceArr[:, :, [1, 0, 2]]
             arr *= target.direction
+            faceArr *= target.direction
 
             # Use the Rodrigues formula to rotate around the target axis.
             x, y, z = target.direction
@@ -106,12 +124,19 @@ class Shape:
                 [x * y + z, 0, y * z - x],
                 [x * z - y, y * z + x, 0],
             ], dtype=int) // 2
-            rot = np.linalg.matrix_power(mat, rotation)
-            arr = np.matmul(arr, rot)
 
-            arr[:, 0] += target.center
+        rot = np.linalg.matrix_power(mat, rotation)
+        arr = np.matmul(arr, rot)
+        faceArr = np.matmul(faceArr, rot)
 
-            return Shape(arr)
+        arr[:, 0] += target.center
+        faceArr[:, 0] += target.center
+
+        faces = {}
+        for i, label in zip(range(0, len(faceArr), 2), self.faces.values()):
+            faces[(Widget(*faceArr[i]), Widget(*faceArr[i + 1]))] = label
+
+        return Shape(arr, faces)
 
     def save(self, path):
         with open(path, 'a') as f:
@@ -128,6 +153,9 @@ def load(path):
             if isinstance(data, list):
                 shapes.append(Shape(data))
             else:
-                shapes[-1].faces = data
+                shapes[-1].faces = {
+                    (Widget(*w), Widget(*x)): label
+                    for (w, x), label in data.items()
+                }
     return shapes
 
