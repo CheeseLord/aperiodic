69074ff4cd2339558624eef3aeb5b842e325f5f5
Kyle Fox <fox_kyle@yahoo.com>
2025-09-01 13:29:44 -0400

    Generate all shapes.

--- octahedral/generate.py
+++ octahedral/generate.py
@@ -18,3 +18,31 @@ def generateRandomShape(n):
 
     return shape.canonical
 
+
+def generateAllShapes(n):
+    if n == 1:
+        return [Shape([((0, 0, 0), (0, 1, 2))])]
+
+    shapes = set()
+    for subshape in generateAllShapes(n - 1):
+        neighbors = set()
+        for w in subshape:
+            neighbors = neighbors.union(w.neighbors)
+        neighbors = neighbors.difference(subshape)
+
+        for neighbor in neighbors:
+            shape = subshape + [neighbor]
+            shapes.add(tuple(sorted(shape, key=tuple)))
+    shapes = list({Shape(s).canonical for s in shapes})
+
+    return shapes
+
+
+if __name__ == '__main__':
+    shapes = []
+    for i in range(1, 8):
+        shapes += generateAllShapes(i)
+
+    for shape in shapes:
+        shape.save('shapes/allShapes.txt')
+
